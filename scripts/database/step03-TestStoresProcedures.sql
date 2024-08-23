d34ba06f9a797268fd55669c3beb82237725723e
Willyams Yujra <yracnet@gmail.com>
2024-08-22 20:02:31 -0400

    + DimShipName, DimShipCountry, DimShipCity, DimShipRegion

--- scripts/database/step03-TestStoresProcedures.sql
+++ scripts/database/step03-TestStoresProcedures.sql
@@ -7,9 +7,9 @@ set @startRow = 0
 set @endRow = dbo.GetCurrentRowVersion ()
 
 -- EXECUTE dbo.GetCategoriesByRowVersion @startRow, @endRow
-EXECUTE dbo.GetCustomersByRowVersion @startRow, @endRow
-EXECUTE dbo.GetEmployeesByRowVersion @startRow, @endRow
+-- EXECUTE dbo.GetCustomersByRowVersion @startRow, @endRow
+-- EXECUTE dbo.GetEmployeesByRowVersion @startRow, @endRow
 -- EXECUTE dbo.GetProductsByRowVersion @startRow, @endRow
 -- EXECUTE dbo.GetSuppliersByRowVersion @startRow, @endRow
 -- EXECUTE dbo.GetShippersByRowVersion @startRow, @endRow
--- EXECUTE dbo.GetOrdersByRowVersion @startRow, @endRow
+EXECUTE dbo.GetOrdersByRowVersion @startRow, @endRow
